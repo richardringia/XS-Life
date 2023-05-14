46920af15d2eeb2ae4d5e21917fc0b4da717d657
Richard Ringia <rringia99@outlook.com>
2023-05-14 13:35:48 +0700

    Created model and repository

--- lib/src/constants/collection_constants.dart
+++ lib/src/constants/collection_constants.dart
@@ -1,4 +1,6 @@
 class CollectionConstants {
   static const forum = "forum";
   static const forumComment = "forum_comment";
+  static const map = "point_of_interests";
+  static const mapCategory = "point_of_interests_category";
 }
