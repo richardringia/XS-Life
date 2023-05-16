44976975a21c1883d8f0f43cf67e12f6dea50933
Pituchai <Phantom_14706@hotmail.com>
2023-05-17 02:46:28 +0700

    (Please Changes before Push)Update from fam
    Stills lefts
    - - [ ]  Forum answer question page
        - [ ]  Make it like the design (Change category to white),(Cards make more shred like figma)

--- lib/src/common_widgets/navigation_fab.dart
+++ lib/src/common_widgets/navigation_fab.dart
@@ -6,23 +6,26 @@ class NavigationFab extends StatelessWidget {
   const NavigationFab({
     super.key,
     required this.location,
-    required this.icon,
+    required this.image,
     this.onPressed,
   });
 
   final String location;
-  final Icon icon;
+  final Image image ;
   final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
+        
           borderRadius: BorderRadius.circular(10),
           color: Color.fromARGB(1, 1, 1, 1)),
+          
       child: Visibility(
         visible: !(ModalRoute.of(context)?.settings.name == location),
         child: Container(
+        
           margin: const EdgeInsets.all(10),
           // icon
           child: FloatingActionButton(
@@ -32,7 +35,7 @@ class NavigationFab extends StatelessWidget {
                   context.replace('/$location');
                 },
             backgroundColor: Color.fromRGBO(255, 175, 54, 1),
-            child: icon,
+            child: image,
           ),
         ),
       ),
--- lib/src/features/forum/presentation/forum_question_card.dart
+++ lib/src/features/forum/presentation/forum_question_card.dart
@@ -7,46 +7,58 @@ import 'package:xs_life/src/features/forum/domain/forum_question.dart';
 class ForumQuestionCard extends StatelessWidget {
   final ForumQuestion forumQuestion;
 
-  const ForumQuestionCard({super.key, required this.forumQuestion});
+  const ForumQuestionCard({
+    Key? key,
+    required this.forumQuestion,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     ForumRepository forumRepository = ForumRepository();
 
-    return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: Text(forumQuestion.userDetail!.getFullName() ?? "Anonymous"),
-          ),
-          Container(
-            padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-            alignment: Alignment.topLeft,
-            child: Text(forumQuestion.question),
-          ),
-          ButtonTheme(
-            child: ButtonBar(
-              alignment: MainAxisAlignment.start,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.thumb_up),
-                  onPressed: () {
-                    forumRepository.likeQuestion(forumQuestion.key);
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.comment),
-                  onPressed: () {
-                    context.push(
-                        '/${PageConstants.forum}/edit/${forumQuestion.key}');
-                  },
-                ),
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0), // Adjust the value to your preference
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0), // Adjust the value to your preference
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: Text(
+                forumQuestion.userDetail!.getFullName() ?? "Anonymous",
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
-          )
-        ],
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+              alignment: Alignment.topLeft,
+              child: Text(forumQuestion.question),
+            ),
+            ButtonTheme(
+              child: ButtonBar(
+                alignment: MainAxisAlignment.start,
+                children: [
+                  // Make it show the number of likes using Firebase
+                  IconButton(
+                    icon: const Icon(Icons.thumb_up),
+                    onPressed: () {
+                      forumRepository.likeQuestion(forumQuestion.key);
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.comment),
+                    onPressed: () {
+                      context.push('/${PageConstants.forum}/edit/${forumQuestion.key}');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
